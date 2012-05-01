da3179c51149c5bbb111cacd0ce285eb30061b84
Toby Inkster <mail@tobyinkster.co.uk>
2012-05-01 19:06:52 +0100

    document type lib

--- lib/Web/Id/Types.pm
+++ lib/Web/Id/Types.pm
@@ -48,3 +48,62 @@ coerce Uri,
 
 
 __PACKAGE__
+__END__
+
+=head1 NAME
+
+Web::Id::Types - type library for Web::Id and friends
+
+=head1 DESCRIPTION
+
+This module uses L<Any::Moose> and is capable of providing either a
+L<MooseX::Types> type library, or a L<MouseX::Types> type library.
+
+=head2 Types
+
+=over
+
+=item * C<Bigint>
+
+=item * C<Certificate>
+
+=item * C<Datetime>
+
+=item * C<Finger>
+
+=item * C<Model>
+
+=item * C<Rsakey>
+
+=item * C<San>
+
+=item * C<Uri>
+
+=back
+
+=head1 BUGS
+
+Please report any bugs to
+L<http://rt.cpan.org/Dist/Display.html?Queue=Web-Id>.
+
+=head1 SEE ALSO
+
+L<Web::Id>.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2012 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
